b7edc4e91d13860a1f857c983eaca179b206f3d0
Microwavekonijn <microwavekonijn@pm.me>
2022-06-19 14:20:03 +0200

    feat: added support for the devkit

--- src/rabbit-mq/rabbit-mq.config.ts
+++ src/rabbit-mq/rabbit-mq.config.ts
@@ -2,11 +2,11 @@ import { ProcessEnv } from '@census-reworked/nestjs-utils';
 import { IsNotEmpty } from 'class-validator';
 
 export class RabbitMqConfig {
-  @ProcessEnv('RMQ_URL')
+  @ProcessEnv('RABBITMQ_URL')
   @IsNotEmpty()
   url = 'amqp://localhost';
 
-  @ProcessEnv('RMQ_EVENT_EXCHANGE')
+  @ProcessEnv('RABBITMQ_EVENT_EXCHANGE')
   @IsNotEmpty()
   eventExchange: string;
 }
