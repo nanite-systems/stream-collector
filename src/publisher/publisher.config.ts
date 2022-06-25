296c0c412e6bdd39d6e6570d246c8c550367fc7c
Microwavekonijn <microwavekonijn@pm.me>
2022-06-25 20:07:10 +0200

    fix: add random id to collector when left empty

--- src/publisher/publisher.config.ts
+++ src/publisher/publisher.config.ts
@@ -1,8 +1,9 @@
 import { ProcessEnv } from '@census-reworked/nestjs-utils';
 import { IsNotEmpty } from 'class-validator';
+import { randomUUID } from 'crypto';
 
 export class PublisherConfig {
   @ProcessEnv('PUBLISHER_ID')
   @IsNotEmpty()
-  publisherId: string;
+  publisherId = randomUUID();
 }
