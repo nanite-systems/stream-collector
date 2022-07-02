2fc664806757d835727c4b1dc2fa5d048bc6b962
Microwavekonijn <microwavekonijn@pm.me>
2022-07-02 23:48:39 +0200

    fix(log): catch and log uncaught exceptions

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigModule } from '@census-reworked/nestjs-utils';
 import { AppConfig } from './app.config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   ConfigModule.forRoot();
@@ -10,6 +11,14 @@ async function bootstrap() {
 
   const app = await NestFactory.createApplicationContext(AppModule, {
     logger: config.logLevels,
+  });
+
+  process.on('uncaughtException', (err) => {
+    const logger = new Logger('UncaughtException');
+
+    logger.error(err, err.stack);
+    app.close();
+    process.exit(1);
   });
 
   app.enableShutdownHooks();
